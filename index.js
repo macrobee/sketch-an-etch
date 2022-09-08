7a2d6ddce2a8e85040473edc0523064f33024124
Vivian T <vivian.tong95@gmail.com>
2022-09-08 17:14:31 -0400

    Add basic black brush functionality

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const canvas = document.querySelector('.canvas');
 let gridWidth = document.querySelector('#dimensions').value;
 
-let canvasWidth = canvas.offsetWidth-45;
-let canvasHeight = canvas.offsetHeight-45;
-console.log({canvasWidth, canvasHeight})
+let canvasWidth = canvas.offsetWidth - 45;
+let canvasHeight = canvas.offsetHeight - 45;
+// console.log({ canvasWidth, canvasHeight })
 let boxSize = pickBoxSize(canvasWidth, canvasHeight, gridWidth);
-let cellsAcross = Math.floor((canvasWidth-20)/boxSize);
+let cellsAcross = Math.floor((canvasWidth - 20) / boxSize);
 
 function pickBoxSize(canvasWidth, canvasHeight, gridWidth) {
     let smallerDimension;
     canvasWidth > canvasHeight ? smallerDimension = canvasHeight : smallerDimension = canvasWidth;
-    return Math.floor(smallerDimension/gridWidth);
+    return Math.floor(smallerDimension / gridWidth);
 }
-console.log(`each box is ${boxSize}px wide and ${boxSize}px high`);
+// console.log(`each box is ${boxSize}px wide and ${boxSize}px high`);
 
 drawGrid(cellsAcross, gridWidth, boxSize);
 
@@ -21,7 +21,7 @@ function drawGrid(width, height, boxSize) {
         let newRow = document.createElement('div');
         newRow.classList.add('canvas-row');
         canvas.appendChild(newRow);
-        console.log('made a row');
+        // console.log('made a row');
         for (let j = 1; j <= width; j++) {
             let newBox = document.createElement('div');
             newBox.classList.add('canvas-box');
@@ -30,5 +30,38 @@ function drawGrid(width, height, boxSize) {
     }
     let boxes = document.querySelectorAll('.canvas-box');
     boxes.forEach(box => box.setAttribute('style', `width: ${boxSize}px; height: ${boxSize}px`))
+    boxes.forEach(box => box.addEventListener('mouseover', brush));
 }
-console.log(canvas);
+
+function brush(e) {
+    let targetDiv = e.target;
+    let targetBackground = targetDiv.style.backgroundColor;
+
+    if (!targetBackground) {
+        targetDiv.style.backgroundColor = targetBackground = `rgb(255,255,255)`
+    } else if (targetBackground == 'rgb(0, 0, 0)') {
+        return
+    }
+
+    let currentColor = targetBackground;
+    let [r, g, b] = getRGBfromString(currentColor);
+    // console.log(`red: ${r}, blue: ${b}, green: ${g}`);
+
+    let newR = darken(r, 50);
+    let newG = darken(g, 50);
+    let newB = darken(b, 50);
+    targetDiv.style.backgroundColor = `rgb(${newR},${newG},${newB})`;
+    // console.log(`now it's ${targetDiv.style.backgroundColor}`)
+}
+
+function getRGBfromString(rgbString) {
+    let numberString = rgbString.slice(4, -1);
+    let rgbArray = numberString.split(",");
+    return rgbArray;
+}
+
+function darken(originalColor, amount) {
+    let newColor;
+    originalColor >= amount ? newColor = originalColor - amount : newColor = 0;
+    return newColor;
+}
